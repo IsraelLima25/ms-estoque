73a8d505f4cfb900151cf075e481416558dac5d3
Israel Santos Lima <israellima@dcc.ufmg.br>
2025-12-12 14:47:53 -0300

    Estrutura criada

---
+++ src/main/java/com/business/project/ms_estoque/MsEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.business.project.ms_estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEstoqueApplication.class, args);
+	}
+
+}
