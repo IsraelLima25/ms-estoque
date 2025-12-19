8d107c2645d91be4aa5620dc138531e009c225d3
Israel Santos Lima <israel_lima_16@hotmail.com>
2025-12-19 19:42:30 -0300

    refactor: mais um comentário adicionado

--- src/main/java/com/business/project/ms_estoque/controller/ProdutoController.java
+++ src/main/java/com/business/project/ms_estoque/controller/ProdutoController.java
@@ -25,7 +25,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 @AllArgsConstructor
 public class ProdutoController {
 
-    // Comentario adicionado
+    // Comentario adicionados
 
     private ProdutoRepository produtoRepository;
     private HistoricoService historicoService;
